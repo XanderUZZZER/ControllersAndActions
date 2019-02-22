c1d65c8e38c5236fb36a05fd70c85ce9b8478f23
XanderUZZZER <xanderuzzzer@gmail.com>
2019-02-22 15:32:24 +0200

    Add project files.

---
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class ExampleController : Controller
+    {
+        //public ViewResult Index() => View(DateTime.Now);
+
+        //public ViewResult Index()
+        //{
+        //    ViewBag.Message = "Hello";
+        //    ViewBag.Date = DateTime.Now;
+        //    return View();
+        //}
+
+        //public JsonResult Index() => Json(new[] { "Alice", "Bob", "Joe" });
+
+        //public ContentResult Index()
+        //    => Content("[\"Alice\",\"Bob\",\"Joe\"]", "application/json");
+
+        //public ObjectResult Index() => Ok(new string[] { "Alice", "Bob", "Joe" });
+
+        //public VirtualFileResult Index()
+        //    => File("/lib/twitter-bootstrap/css/bootstrap.css", "text/css");
+        public StatusCodeResult Index()
+                => StatusCode(StatusCodes.Status404NotFound);
+
+        public ViewResult Result() => View((object)"Hello World");
+
+        //public RedirectResult Redirect() => Redirect("/Example/Index");
+
+        //public RedirectToRouteResult Redirect() =>
+        //    RedirectToRoute(new
+        //    {
+        //        controller = "Example",
+        //        action = "Index",
+        //        ID = "MyID"
+        //    });
+
+        public RedirectToActionResult Redirect()
+            => RedirectToAction(nameof(HomeController.Index), 
+                                nameof(HomeController));
+    }
+}
---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using ControllersAndActions.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() => View("SimpleForm");
+
+        //public ViewResult ReceiveForm(string name, string city)
+        //{
+        //    return View("Result", $"{name} lives in {city}");
+        //}
+
+        //public void ReceiveForm(string name, string city)
+        //{
+        //    Response.StatusCode = 200;
+        //    Response.ContentType = "text/html";
+        //    byte[] content = Encoding.ASCII.GetBytes($"<html><body>{name} lives in {city}</body>");
+        //    Response.Body.WriteAsync(content, 0, content.Length);
+        //}
+
+        //public IActionResult ReceiveForm(string name, string city)
+        //    => new CustomHtmlResult
+        //    {
+        //        Content = $"{name} lives in {city}"
+        //    };
+
+        //public ViewResult ReceiveForm(string name, string city)
+        //    => View("Result", $"{name} lives in {city}");
+
+        [HttpPost]
+        public RedirectToActionResult ReceiveForm(string name, string city)
+        {
+            TempData["name"] = name;
+            TempData["city"] = city;
+            return RedirectToAction(nameof(Data));
+        }
+        
+        public ViewResult Data()
+        {
+            string name = TempData["name"] as string;
+            string city = TempData["city"] as string;
+            return View("Result", $"{name} lives in {city}");
+        }
+    }
+}
